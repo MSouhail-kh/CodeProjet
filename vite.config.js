3491a6f8dd7be0d34efce0a7f864eac167d2e2f9
M.Souhail-Khalal <hgh765469@gmail.com>
2025-02-28 08:30:53 +0000

    Update vite.config.js
    Signed-off-by: M.Souhail-Khalal <hgh765469@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
-export default defineConfig({
+import { defineConfig } from 'vite';  
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({  
   plugins: [react()],
   base: '/',
   build: {
@@ -6,8 +9,7 @@ export default defineConfig({
     assetsDir: 'assets'
   },
   server: {
-    historyApiFallback: true,
-    middlewareMode: true
+    historyApiFallback: true
   },
-  publicDir: 'public' 
+  publicDir: 'public'
 });
