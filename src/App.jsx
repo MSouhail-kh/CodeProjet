91517a4c887137720d8d80c30c8ee3d96ced78f9
M.Souhail-Khalal <hgh765469@gmail.com>
2025-02-28 22:28:12 +0000

    Update App.jsx
    Signed-off-by: M.Souhail-Khalal <hgh765469@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { AuthProvider } from "./Authentification/AuthContext";
 import PrivateRoute from "./Authentification/PrivateRoute";
 import LoginSignup from "./Authentification/LoginSignup";
 import Chaines from "./Chaines/Chaines";
 import ProduitDetails from "./Produits/ProduitDetails";
 import EnhancedSwiper from "./Swiper/Swiper";
+import Refresh from "./Refresh"; // Importer le composant Refresh
 import './App.css';
+
 function App() {
     return (
         <AuthProvider>
             <Router>
+                <SaveLastPage /> {/* Garde en mémoire la dernière page */}
                 <Routes>
                     <Route path="/" element={<LoginSignup />} />
                     <Route path="/Chaines" element={<PrivateRoute element={<Chaines />} />} />
@@ -26,10 +29,20 @@ function App() {
                             />
                         }
                     />
+                    <Route path="/refresh" element={<Refresh />} />
                 </Routes>
             </Router>
         </AuthProvider>
     );
 }
+
+// Composant qui sauvegarde la dernière page visitée
+const SaveLastPage = () => {
+    const location = useLocation();
+    useEffect(() => {
+        sessionStorage.setItem("lastPage", location.pathname);
+    }, [location]);
+    return null;
+};
 
 export default App;
