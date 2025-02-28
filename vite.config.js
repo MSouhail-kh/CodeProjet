e603b6120718525ce17bbb13bf6c9d4c6d8232f1
souhail <hgh765469@gmail.com>
2025-02-28 17:25:12 +0000

    V20

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', 
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+    outDir: 'dist',  
+    assetsDir: 'assets' 
   },
   server: {
-    historyApiFallback: true 
+    historyApiFallback: true
   },
-  publicDir: 'public'
+  publicDir: 'public' 
 });
