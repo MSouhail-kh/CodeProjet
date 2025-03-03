e38a84f2a1e3c5967c0b1ce9dfe452cb319c7577
M.Souhail-Khalal <hgh765469@gmail.com>
2025-03-03 23:03:17 +0000

    Delete electron directory
    Signed-off-by: M.Souhail-Khalal <hgh765469@gmail.com>

--- electron/preload.js
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld("electron", {
-    confirmDialog: (message) => ipcRenderer.invoke("confirm-dialog", message),
-    promptDialog: (message) => ipcRenderer.invoke("prompt-dialog", message),
-});
