0a0b9fbfc691a1c78c1b73017fda6e0fce7c066f
souhail <hgh765469@gmail.com>
2025-03-11 14:38:16 +0000

    Gestion_Planning_with_websocket_V44

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -31,13 +31,13 @@ import './Navbar.css';
             <div className="d-flex flex-grow-1 justify-content-between align-items-center">
             <Form className="d-flex mx-4 my-2 my-lg-0 flex-grow-1 justify-content-center">
               <InputGroup className="search-group align-items-center">
-                <InputGroup.Text className="search-icon ">
-                  <Search className="mx-1" />
+                <InputGroup.Text className="search-icon p-0 border-0 bg-transparent">
+                  <Search className="mx-4" />
                 </InputGroup.Text>
                 <Form.Control
                   type="search"
                   placeholder="Rechercher..."
-                  className="search-input border-0 py-3"
+                  className="search-input border-0 py-2"
                   aria-label="Search"
                 />
               </InputGroup>
