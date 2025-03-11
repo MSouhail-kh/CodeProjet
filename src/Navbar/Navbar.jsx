7d75aadb81d6861bd61480037f57f314772e70a3
souhail <hgh765469@gmail.com>
2025-03-11 14:56:00 +0000

    Gestion_Planning_with_websocket_V44

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -46,10 +46,10 @@ import './Navbar.css';
 
               <Nav className="align-items-center">
                 <Nav.Link className="btn gradient-btn btn-lg me-2" onClick={handleShowProduit}>
-                  <PlusCircle className="icon-btn " /> 
+                  <PlusCircle className="icon-btn " size={28}  /> 
                 </Nav.Link>
                 <Nav.Link className="btn gradient-btn  btn-lg " onClick={handleShowExcel}>
-                  <FileEarmarkPlus className="icon-btn" /> 
+                  <FileEarmarkPlus className="icon-btn" size={28} /> 
                 </Nav.Link>
                 <Nav.Item className="ms-3">
                   <UserProfile />
