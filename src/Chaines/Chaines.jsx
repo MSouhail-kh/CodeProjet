a3d1e4183af9254e01b82a27706211c4a4bb6d8b
souhail <hgh765469@gmail.com>
2025-03-14 13:28:35 +0000

    Gestion_Planning_with_websocket_V51

--- src/Chaines/Chaines.jsx
+++ src/Chaines/Chaines.jsx
@@ -40,7 +40,7 @@ const Dot = styled.div`
 `;
 
 const StyledCard = styled(Card)`
-  height: 100%;
+  height: 50%;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   transition: transform 0.2s, box-shadow 0.2s;
 
