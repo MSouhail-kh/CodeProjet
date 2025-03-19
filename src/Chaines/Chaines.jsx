9dd7363db3c84ed1e89468c9fd36da916b4efe30
souhail <hgh765469@gmail.com>
2025-03-19 10:20:17 +0000

    Gestion_Planning_V49

--- src/Chaines/Chaines.jsx
+++ src/Chaines/Chaines.jsx
@@ -4,7 +4,7 @@ import { Container, Row, Col, Card, ListGroup, Button } from "react-bootstrap";
 import { CheckCircle } from "react-bootstrap-icons";
 import MyNavbar from "../Navbar/Navbar";
 import { useNavigate } from "react-router-dom";
-import NoImage from "../assets/NoImage.png";
+import NoImage from "../assets/No+Image.png";
 import api from "../services/axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
