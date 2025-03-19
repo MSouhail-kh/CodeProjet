dbcd6e1694289b4fca8afaad15933761ed55cbac
souhail <hgh765469@gmail.com>
2025-03-19 08:22:42 +0000

    Gestion_Planning_V42

--- src/Chaines/Chaines.jsx
+++ src/Chaines/Chaines.jsx
@@ -236,7 +236,7 @@ const ChainColumn = ({
                   onMouseLeave={handleMouseLeave}
                 >
                   <ProductContainer>
-                    <ProductImage src={item.image} alt={item.style} />
+                    <ProductImage src={hoveredItem.image || NoImage} alt={item.style} />
                     <ProductStyle>{item.style}</ProductStyle>
                   </ProductContainer>
                 </StyledListGroupItem>
